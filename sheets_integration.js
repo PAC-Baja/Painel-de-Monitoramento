dd902cbf8ba25e8b097f526a362179a7ac72ee5a
pacbaja <78366240+pacbaja@users.noreply.github.com>
2021-06-18 10:00:03 -0300

    Update sheets_integration.js

--- sheets_integration.js
+++ sheets_integration.js
@@ -1,7 +1,7 @@
   
   // Referencia da planilha
-  //var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1AGAyPfcMfp7X5CLWJGvKqYDM1ML0U-KIwu0gAPIiwkk/edit?usp=sharing';
-  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1u83fH2VDUy2f0_eT8qsLRoTIoefQfh4O3M0SQouQ5uk/edit?usp=sharing';
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1AGAyPfcMfp7X5CLWJGvKqYDM1ML0U-KIwu0gAPIiwkk/edit?usp=sharing';
+  //var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1u83fH2VDUy2f0_eT8qsLRoTIoefQfh4O3M0SQouQ5uk/edit?usp=sharing';
 
  
   // Função que puxa dados da planilha
